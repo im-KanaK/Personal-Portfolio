6a88d6dec2d4aa4822798f5325fd30e1a609529a
Kanak Dubey <126403840+im-KanaK@users.noreply.github.com>
2023-04-06 22:21:44 +0530

    Add files via upload

---
+++ client/src/PortfolioContainer/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="colz-icon">
+        <a href="https://www.facebook.com/kanak.dubey.923724?mibextid=ZbWKwL">
+          <i className="fa fa-facebook-square"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/kanakdubey">
+          <i className="fa fa-instagram"></i>
+        </a>
+        <a href="https://twitter.com/_kanakdubey">
+          <i className="fa fa-twitter"></i>
+        </a>
+        <a href="https://github.com/im-KanaK">
+          <i className="fa fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/kanakdubey">
+          <i className="fa fa-linkedin"></i>
+        </a>
+      </div>
+      <h4>All Rights Reserved.</h4>
+    </div>
+  );
+};
+
+export default Footer;
